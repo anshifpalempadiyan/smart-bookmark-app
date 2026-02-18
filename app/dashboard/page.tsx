b1cf3df0e50da9b5272b6882c167250a3cbafdc1
anshif-8590 <anshifp949@gmail.com>
2026-02-18 12:22:02 +0530

    added the logout button and funtionality

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -5,11 +5,11 @@ import { supabase } from "../lib/supabaseClient"
 
 
 const DashboardPage = () => {
-    const [ user , setUser ] = useState<any>(null)
+    const [user, setUser] = useState<any>(null)
 
     useEffect(() => {
         supabase.auth.getUser().then(({ data }) => {
-            if ( !data.user ) {
+            if (!data.user) {
                 window.location.href = "/login"
             } else {
                 setUser(data.user)
@@ -17,7 +17,12 @@ const DashboardPage = () => {
         })
     }, [])
 
-    if ( !user ) {
+    const handleLogout = async () => {
+        await supabase.auth.signOut()
+        window.location.href = "/login"
+    }
+
+    if (!user) {
         return (
             <p className="p-4">Loading...</p>
         )
@@ -25,9 +30,15 @@ const DashboardPage = () => {
 
     return (
         <main className="p-6">
-            <h1 className="text-xl font-semibold ">
-                Welcome, {user.email}
-            </h1>
+            <div className="flex items-center justify-between">
+
+                <h1 className="text-xl font-semibold ">
+                    Welcome, {user.email}
+                </h1>
+                <button className="reounded bg-red-600 px-4 py-2 text-white " onClick={handleLogout}>
+                    Logout
+                </button>
+            </div>
         </main>
     )
 }
